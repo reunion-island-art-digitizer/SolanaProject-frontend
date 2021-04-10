6d5863f11b35094afaf09ac22ea3f39bb0109a22
tangying <1020278488@qq.com>
2021-04-10 12:22:07 +0800

    img fix

--- vue.config.js
+++ vue.config.js
@@ -24,7 +24,7 @@ module.exports = {
   devServer: {
     proxy: {
       "/api": {
-        target: "http://apidev.xinchain.org/apiSolana/", // 域名
+        target: "https://api.staked.xyz/apiSolana/", // 域名
         ws: true, // 是否启用websockets
         changOrigin: true, //开启代理：在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
         pathRewrite: {
